4f543eefe1d0b462a81557f5939ed9dcac843bdc
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2016-05-19 08:10:43 +0530

    added error handling

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"errors"
+	//"error"
 	//	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -36,13 +36,25 @@ func main() {
 }
 
 func renderTemplate(w http.ResponseWriter, p *Page, tmpl string) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	// Got to make sure that err is not nil in case of err
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, p, "view")
 	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
@@ -57,5 +69,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/", http.StatusFound)
 }
